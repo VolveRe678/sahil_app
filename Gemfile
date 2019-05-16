f76d7ff8980b9f2db2ecc58f216d5944d5091ab2
Sahil Maharjan <sahil.maharjan678@gmail.com>
2019-05-16 21:51:42 +0545

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'jquery-rails'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'faker'
+gem 'will_paginate',           '3.1.7'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Sahil Z Maharjan",
+    email: "sahil.maharjan678@gmail.com",
+    password:              "Harisiddhi",
+    password_confirmation: "Harisiddhi",
+    admin: true)
+
+99.times do |n|
+name  = Faker::Name.name
+email = "example-#{n+1}@railstutorial.org"
+password = "password"
+User.create!(name:  name,
+      email: email,
+      password:              password,
+      password_confirmation: password)
+end
