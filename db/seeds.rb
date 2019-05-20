61414f720db71aa47635a7d92c41819840995610
Sahil Maharjan <sahil.maharjan678@gmail.com>
2019-05-20 14:17:38 +0545

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,9 @@ password = "password"
 User.create!(name:  name,
       email: email,
       password:              password,
-      password_confirmation: password)
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
@@ -29,3 +31,10 @@ users = User.order(:created_at).take(6)
     content = Faker::Lorem.sentence(5)
     users.each{|user| user.microposts.create!(content: content)}
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
